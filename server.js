ce2b55f682293b7bc8c8f1766e76959973997307
Cameron LaVallie <camlavallie@gmail.com>
2019-06-03 22:12:40 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect DB
+connectDB();
+
+// Init middleware
+
+app.use(express.json({extended: false}))
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+
+app.use('/api/users', require('./routes/API/users'));
+app.use('/api/auth', require('./routes/API/auth'));
+app.use('/api/profile', require('./routes/API/profile'));
+app.use('/api/posts', require('./routes/API/posts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
